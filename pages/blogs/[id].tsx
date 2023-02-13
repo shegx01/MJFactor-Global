700fd85f827a4334c1c9c464c592a088a8e0bcdb
shegx01 <shegx01@gmail.com>
2023-02-13 08:11:06 +0200

    removed console logs

--- pages/blogs/[id].tsx
+++ pages/blogs/[id].tsx
@@ -1,16 +1,13 @@
+import React, { useEffect, useState } from "react";
 import { Box, Toolbar } from "@mui/material";
 import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
 import blogsData from "../../src/blogsData";
 // import { GetStaticProps, GetStaticPropsContext } from 'next'
 
-
 function getBlogData(id: string) {
-    console.log('blogdata id', id)
   const specified_target = `/blogs/${id}`;
   const blog = blogsData.find(({ target }) => target === specified_target);
-console.log('blog:', blog)
-  return blog || null
+  return blog || null;
 }
 
 type BlogData = {
@@ -23,20 +20,20 @@ type BlogData = {
 };
 
 const Blog: React.FC<BlogData> = () => {
-    const router = useRouter()
-    const [id, setId] = useState(router.query.id || "")
-    const [blog, setBlog] = useState(null)
+  const router = useRouter();
+  const [id, setId] = useState(router.query.id || "");
+  const [blog, setBlog] = useState(null);
 
-    useEffect(() => {
-        setId(router.query.id as string)
-        setBlog(getBlogData(router.query.id as string))
-    },[router.query.id])
-  return <Box>
-    <Toolbar />
-        {
-            blog && JSON.stringify(blog)
-        }
-    </Box>;
+  useEffect(() => {
+    setId(router.query.id as string);
+    setBlog(getBlogData(router.query.id as string));
+  }, [router.query.id]);
+  return (
+    <Box>
+      <Toolbar />
+      {blog && JSON.stringify(blog)}
+    </Box>
+  );
 };
 
 export default Blog;
